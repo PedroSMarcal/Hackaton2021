83d87a1344659def6a68e12bf8ead82b9a79fcc1
Pedro Hnerique Silva <pedro.h.silva.marcal@gmail.com>
2021-07-18 12:12:09 -0300

    emailtest

--- utils/ImagesUtils.py
+++ utils/ImagesUtils.py
@@ -1,13 +1,18 @@
-from models.models import Photos
+from models.models import Photos, Occurrence
 
-def contructImages(f, data):
-    try: 
-        new_Photo = Photos({
-            "name": f.filename, 
-            "occurrence_id": data['occurrence_id']
-        })
-        f.save(secure_filename(f.filename))
-        response = {'message': 'upload complete'}
-    except:
-        reponse = {'message': 'cant upload image'}
+import os
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+def contructImages(f):
+    obj = Occurrence.query(ObjectRes).order_by(ObjectRes.id.desc()).first()
+    new_Photo = Photos({
+        "name": f.filename, 
+        "occurrence_id": obj.id
+    })
+    f.save(secure_filename(f.filename))
+    response = {'message': 'upload complete'}
+
+def getImages(id):
+    obj = Photos.query.filter_by(id=id)
+    return obj
